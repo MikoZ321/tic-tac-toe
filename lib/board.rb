4a4dd28661939bd673031a0b38bfc38d7a40d3e0
MikoZ321 <0123456789miko@gmail.com>
2026-05-02 22:25:52 +0200

    add basic project structure

---
+++ lib/board.rb
@@ -0,0 +1,5 @@
+class Board
+  def initialize
+    @grid = Array.new(3) {|row| row = Array.new(3)}
+  end
+end
---
+++ lib/game.rb
@@ -0,0 +1,6 @@
+class Game 
+  def initialize
+    @turn = 0
+    @board = Board.new()
+  end
+end
